c3d55747aadf1aead312475e895489c57a06763b
Lena Sabadina <elena.sabadina@icloud.com>
2019-08-27 15:34:30 +0200

    Add NSCoder instead of userDefaults to store our array of objects instead of array of String.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -11,26 +11,14 @@ import UIKit
 class TodoListViewController: UITableViewController {
 
     var itemArray = [Item]()
-    let defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let newItem = Item()
-        newItem.title = "Shopping list"
-        itemArray.append(newItem)
+        print(dataFilePath)
         
-        let newItem2 = Item()
-        newItem2.title = "Work"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "Trip"
-        itemArray.append(newItem3)
-        
-            if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
-                itemArray = items
-            }
+        loadItems()
     }
     
     //MARK: TableView Datasource methods
@@ -52,9 +40,8 @@ class TodoListViewController: UITableViewController {
     // MARK: TableView Delegate methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        itemArray[indexPath.row].done != itemArray[indexPath.row].done
-        
-        tableView.reloadData()
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        self.saveItems()
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
@@ -69,8 +56,7 @@ class TodoListViewController: UITableViewController {
             newItem.title = textField.text!
             
            self.itemArray.append(newItem)
-           self.defaults.set(self.itemArray, forKey: "TodoListArray")
-           self.tableView.reloadData()
+           self.saveItems()
         }
         alert.addTextField { (alertTextField) in   // adding the text field in the popup alert message
             alertTextField.placeholder = "Create new item"
@@ -78,6 +64,29 @@ class TodoListViewController: UITableViewController {
         }
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+    }
+    
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+            itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array, \(error)")
+            }
+        }
     }
     
 }
